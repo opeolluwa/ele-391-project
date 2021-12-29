9f9f14f1c76b64f525bc23d9c0c1531500b5766d
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-30 00:30:06 +0100

    UPDATE

--- server/controllers/attendance.js
+++ server/controllers/attendance.js
@@ -5,4 +5,10 @@ const jwt = require("./../utils/jwt")
 function mobile_endpoint(re, res, next) {
     const { week } = req.body;
     //TODO: get data based on week from database and end()
+    database.promise()
+        .query(`SELECT * FROM attendance WHERE week_${week} = ?`, [week])
+        .then(([rows, fields]) => {
+            //send back the week and the data required 
+            return res.send({ message: { rows, week } })
+        })
 }
